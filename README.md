0130738e9aea8d8714b9c02e193cfafee998de18
Kasim Tasdemir <kasimtasdemir@gmail.com>
2023-04-20 12:17:10 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,92 @@
+# Hybrid Features Extraction Approach using Natural Language Processing for Improved Detection of SQL Injection Attack
+This is the implementation of "Hybrid Features Extraction Approach using Natural Language Processing for Improved Detection of SQL Injection Attack" algorithm. It can load the custom datasets, train various models and demonstrate their inference performances. 
+<img width="741" alt="cascade" src="https://user-images.githubusercontent.com/6195512/232533613-5eb44761-1967-43e8-91a0-b064a6513107.png">
+![image](https://user-images.githubusercontent.com/6195512/232533427-62c1af87-194e-497c-8780-f00a13b0337c.png)
+
+
+## Requirements  
+
+- (recommended) Google Colab account, or any other Jupyter Notebook with GPU support.
+- (optional) If you want to run on local machines, use [Mambaforge](https://github.com/conda-forge/miniforge#mambaforge). Mamba package search is significantly faster than Anaconda (or [Anaconda](https://www.anaconda.com/products/distribution) environment)
+## Setup
+### For Google Colab setup
+- Upload all GitHub files into your Google Drive (e.g. '/content/drive/MyDrive/Akademik/Research and Projects/Sakir Hoca Projects/AI Security Intelligence/Codes/20230331_sqli_colab')
+- Update the hardcoded paths and run 'main.ipynb'
+### (optional) For local setup 
+You can skip this part if you use Google Colab. 
+
+The first part (Classical ML methods) can run on virtual Python environment on Win, Mac or Linux.
+
+- Download and install the [Mambaforge](https://github.com/conda-forge/miniforge#mambaforge) environment.
+- Open Miniforge Prompt and change current directory to the project folder. 
+- (Optional) If you have GPU and want to use it update the yml file to contain "tensorflow-gpu".(this might not work. Not tested)
+- Run the following command in the folder, where **rafi-sqli.yml** file resides. This will create a new Python environment with the required packages:
+    -  ``` mamba env create -f rafi-sqli.yml ```
+- Activate the environment
+    - ``` mamba activate rafi-sqli ```
+- Install the following packages:
+    - ``` pip install -q -U "tensorflow-text==2.8.*" ```
+    - ``` pip install -q tf-models-official==2.7.0 ```
+    - (Note: these packages were not in conda repo. If you find it in the repo, use mamba install instead of pip)
+
+
+## Running
+### Running all
+- Modify config.ini file and run 'main.ipynb'
+
+### (Optional) Running only the classical ML based methods
+- Activate rafi-sqli environment in the Miniforge prompt and run the test:
+    - ``` python run_classical_MLs.py -o <output file path>```
+
+
+## Demonstrating the experimental results
+
+- Modify ``` utils\Demonstrate_test_results.ipynb ``` to point the result file and run it.
+
+## Folder contents
+- Main folders: (datasets, utils, trained_models, results)
+  - datasets: SQLi csv file with two columns:'payload' and 'label'.
+  - config.ini: choose the models to be tested and other options. Note: Ensemble models need all classical MLs to be run before.
+  - utils   
+    - Demonstrate test results : produce all visuals and tables used in the paper.
+
+## Troubleshot
+
+- 
+## Release notes
+- Release (v0.5.0)
+  - Adaptive method is implemented. XGboost is being trained with 5000x positive weight, 0.05 threshold in inference.
+  - The proposed method is added to the test results (Tables and Figures)
+  - Supports multiple random seeds in config.ini file.
+- Release (v0.4.0)
+  - OOP is used to modularize the code.
+  - Most of the settings can be set from config.ini file.
+  - Classic ML and Transformers can be run with a single file (notebook)
+  - "results demonstration" file is updated
+  - results are saved into CSV file, not pkl.
+  - "saving models" does NOT work.
+- Release (0.3.0) Latex table generation
+  - The results are saved to a pkl file in the main folder.
+  - the results pickle files can be read and visualized using utils\Demonstrate_test_results.ipynb. This also generates the Latex tables used in the paper.
+  - the visualized results have color scheme. The fonts sizes, etc. are ready for the paper.
+- Release (v0.2.0)
+    - BERT is added to the tests. See ``` package/detect_sqi_with_transformers.ipynb ```
+    - ``` utils\Demonstrate_test_results.ipynb ``` combine the results and demonstrates the experimental results.
+- Release (v0.1.0)
+    - Results are saved to a Pandas dataframe. It is saved to a pickle file.
+    - Results can be visualized using Utils/Data visualize . jpy notebook.
+    - The original code required datasets with 'delimiter=three tabs'. This is no longer supported by Pandas data frame. So, It has been changed to support single tab delimited dataset. If you need to use the old code on the old datasets, you can use Release v0.0.12
+	- utils/clean-kaggle-sqli-dataset-and-split.ipynb file is created for cleaning Kaggle SQLi dataset and splitting it into train-test files.
+	- utils/convert-old-dataset-to-new-single-tab.ipynb file is created for converting old three tabs delimited dataset to single tab.
+	- utils/dataset-train-test-splitter.ipynb file is created for splitting the given dataset to train and test parts.
+	- support for running without building the package is added (nlp_hybrid.py)
+
+- Release v0.0.12
+
+    - This is the original , the first code from Rafi. It works with Python 3 (the very first one was Python 2)
+
+## TODO
+- Save the trained models. Add a method to load and run the saved models without training.
+
+
+ 
---
+++ experiments.py
@@ -0,0 +1,310 @@
+from sklearn.metrics import roc_curve, auc, accuracy_score, f1_score, precision_score, recall_score, confusion_matrix
+import matplotlib.pyplot as plt
+
+import time
+from datetime import datetime
+from pathlib import Path
+import pandas as pd
+import numpy as np
+
+from templates import FeatureExtractor, logger
+from classical_models import Classical_Model
+from ensemble_models import Ensemble_1, Ensemble_2, Ensemble_4
+
+def evaluate(y_test, y_pred, notes):
+  accuracy = accuracy_score(y_test, y_pred)
+  precision = precision_score(y_test, y_pred)
+  recall = recall_score(y_test, y_pred)
+  f1 = f1_score(y_test, y_pred)
+  tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
+
+  result = {
+    'accuracy': accuracy,
+    'precision': precision,
+    'recall': recall,
+    'f1_score': f1,
+    'tp': tp,
+    'tn': tn,
+    'fp': fp,
+    'fn': fn
+  }
+
+  result.update(notes)
+  return result
+
+
+def save_results(results_dict, dest_file, *args, **kwargs):
+  header=True
+  for key, value in kwargs.items():
+    print("{} is {}".format(key,value))
+    if key == 'header':
+      header=value
+  
+  results_df = pd.DataFrame(results_dict)
+  if dest_file.is_file():
+    df = pd.read_csv(dest_file)
+    results_df = pd.concat([df, results_df])
+    print('Appending to the existing .csv file.')
+
+  results_df.to_csv(dest_file,  index=False, header=header) 
+
+class TestManager:
+  def __init__(self, data_manager, config, output_file_name=''):
+    self.data_manager = data_manager
+    self.results = []
+    self.config = config
+    self.feature_extractors_dict = {}
+    self.models_dict = {}
+    self.output_file_name = output_file_name
+
+  def __evaluations(self, y_test, y_pred, model, feature_extractor):
+    notes = {
+      'feature_method': model.feature_method,
+      'model': model.model_name
+    }
+
+    notes.update(self.data_manager.notes)
+    notes.update(feature_extractor.notes)
+    notes.update(model.notes)
+    notes.update({'dataset': self.config['dataset']['file']})
+    self.results.append(evaluate(y_test, y_pred, notes))
+    
+
+  def __features_models_cartesian_tests(self, feature_methods, models):
+    for feature_method in feature_methods:
+      feature_extractor = FeatureExtractor(feature_method)
+      feature_extractor.extract_features(
+        self.data_manager.x_train, self.data_manager.x_test)
+      self.feature_extractors_dict.update({feature_extractor.method: feature_extractor})
+
+      for model_name in models:
+        model = Classical_Model(model_name)
+        model.feature_method = feature_extractor.method
+        model.fit(
+          feature_extractor.features['train'], self.data_manager.y_train)
+        
+        if model.model_name not in self.models_dict.keys():
+          self.models_dict[model.model_name] = {}
+        self.models_dict[model.model_name].update({feature_extractor.method: model})
+        
+        # Save the trained model
+        if int(self.config['settings']['save_models']) != 0:
+          timestamp = int(time.time())
+          file_name = (Path(self.config['models']['dir']) 
+                      / f"{model.model_name}_{feature_extractor.method}_{timestamp}.pkl")
+          model.save_model(file_name)
+
+        y_pred = model.predict(feature_extractor.features['test'])
+        self.__evaluations(self.data_manager.y_test, y_pred, model, feature_extractor)
+        
+
+
+  def __run_ensemble_tests(self, ensemble_models):
+    for ensemble_model in ensemble_models:
+      model = None
+      feature_extractor = None
+      if ensemble_model == 'ensemble_1':
+        model = Ensemble_1(self.data_manager , self.models_dict, self.feature_extractors_dict)
+        model.feature_method = 'tf-idf, tf-idf_ngram, bag_of_characters'
+        feature_extractor = FeatureExtractor('ensemble_1') # dummy feature ext. just for keeping latency notes.
+      elif ensemble_model == 'ensemble_2':
+        model = Ensemble_2(self.data_manager , self.models_dict, self.feature_extractors_dict)
+        model.feature_method = 'tf-idf, tf-idf_ngram, bag_of_characters'
+        feature_extractor = FeatureExtractor('ensemble_2') # dummy feature ext. just for keeping latency notes.
+      elif ensemble_model == 'ensemble_4':
+        model = Ensemble_4(self.data_manager , self.models_dict, self.feature_extractors_dict)
+        model.feature_method = 'tf-idf, tf-idf_ngram, bag_of_characters'
+        feature_extractor = FeatureExtractor('ensemble_4') # dummy feature ext. just for keeping latency notes.
+      elif ensemble_model == '':
+        print('No ensemble method selected in config.ini file.')
+        continue
+      else:
+        raise ValueError(f"Unknown ensemble model: {ensemble_model}")
+      
+      
+      # self.data_manager.x_train, self.data_manager.x_test
+      model.fit(self.data_manager.x_train, self.data_manager.y_train)
+      y_pred = model.predict(self.data_manager.x_test)
+      feature_extractor.notes = {
+          'extraction_time': model.feature_latency,
+          'feature_size': model.feature_size,
+      }
+      self.model = model #this will be the saved pickle file
+      self.__evaluations(self.data_manager.y_test, y_pred, model, feature_extractor)
+      # Save the trained model
+      if int(self.config['settings']['save_models']) != 0:
+        timestamp = int(time.time())
+        file_name = (Path(self.config['models']['dir']) 
+                    / f"{model.model_name}_{feature_extractor.method}_{timestamp}.pkl")
+        model.save_model(file_name) #doesn't work
+
+  def __adaptive(self, selected_feature_method, selected_model_name, threshold=0.5):
+   
+    feature_extractor = FeatureExtractor(selected_feature_method)
+    feature_extractor.extract_features(
+      self.data_manager.x_train, self.data_manager.x_test)
+    self.feature_extractors_dict.update({feature_extractor.method: feature_extractor})
+
+ 
+    model = Classical_Model(selected_model_name)
+    model.feature_method = feature_extractor.method
+    model.fit(
+      feature_extractor.features['train'], self.data_manager.y_train)
+    
+    if model.model_name not in self.models_dict.keys():
+      self.models_dict[model.model_name] = {}
+    self.models_dict[model.model_name].update({feature_extractor.method: model})
+        
+    # Save the trained model
+    if int(self.config['settings']['save_models']) != 0:
+      timestamp = int(time.time())
+      file_name = (Path(self.config['models']['dir']) 
+                  / f"{model.model_name}_{feature_extractor.method}_{timestamp}.pkl")
+      model.save_model(file_name)
+
+    y_pred = model.predict(feature_extractor.features['test'], threshold=threshold)
+    self.__evaluations(self.data_manager.y_test, y_pred, model, feature_extractor)
+    return model      
+
+  def __save_results(self, dir):
+    if self.output_file_name == '':
+      currentDateAndTime = datetime.now()
+      currentTime = currentDateAndTime.strftime("%y%m%d_%H%M%S")
+      file_name = Path(dir) / f'results_{currentTime}.csv'
+    else:
+      file_name = Path(dir) / Path(self.output_file_name).name
+    
+    self.output_file_name = file_name
+    save_results(self.results, dest_file=self.output_file_name, header=True)
+    logger.info(f"Results saved to {self.output_file_name}")
+
+  def __plot_roc(self, file_name):
+    # predict probabilities of positive class for the test set
+    y_test, y_pred_prob = self.__load_pred_prob(file_name)
+    #y_pred = (y_pred_prob > 0.001).astype(int)
+    #tn, fp, fn, tp = confusion_matrix(self.data_manager.y_test, y_pred).ravel()
+    #print(tn, fp, fn, tp) # 2287 1562 1 2272
+
+    # calculate the false positive rate and true positive rate for different thresholds
+    fpr, tpr, thresholds = roc_curve(y_test, y_pred_prob)
+    roc_auc = auc(fpr, tpr)
+
+    # plot the ROC curve
+    plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic')
+    plt.legend(loc="lower right")
+    plt.show()
+  
+  def __plot_roc2(self, file_name):
+    # predict probabilities of positive class for the test set
+    y_test, y_pred_prob = self.__load_pred_prob(file_name)
+
+
+    # set the threshold range and step size
+    threshold_range = np.arange(0, 0.1, 0.001)
+
+    # calculate the false negative rate for each threshold
+    fpr, tpr, thresholds = roc_curve(y_test, y_pred_prob)
+    fnr = 1 - tpr
+
+    # select the false negative rates for the given threshold range
+    fnr_range = [fnr[np.argmin(np.abs(thresholds - t))] for t in threshold_range]
+
+    # plot the ROC curve for false negatives
+    plt.plot(threshold_range, fnr_range, label='False Negatives')
+
+    # plot the reference line at 50% false negatives
+    plt.plot([0, 1], [0.5, 0.5], linestyle='--', color='gray')
+
+    # plot the focus area with a red rectangle
+    plt.fill_between(threshold_range, fnr_range, where=(threshold_range >= 0) & (threshold_range <= 0.1), alpha=0.2, color='red')
+
+    # set the plot title and axis labels
+    plt.title('ROC Curve for False Negatives')
+    plt.xlabel('Threshold')
+    plt.ylabel('False Negative Rate')
+
+    # set the plot limits and legend
+    plt.xlim([0, 1])
+    plt.ylim([0, 1])
+    plt.legend()
+
+    # show the plot
+    plt.show()
+
+  def __plot_roc3(self, file_name):
+    # predict probabilities of positive class for the test set
+    y_test, y_pred_prob = self.__load_pred_prob(file_name)
+
+
+    # Define the threshold range
+    threshold_range = np.arange(0, 0.1, 0.001)
+
+    # Calculate the confusion matrix for each threshold value
+    fp_rate_list = []
+    fn_rate_list = []
+    for threshold in threshold_range:
+      y_pred_binary = (y_pred_prob >= threshold).astype(int)
+      tn, fp, fn, tp = confusion_matrix(y_test, y_pred_binary).ravel()
+      fp_rate = fp / (fp + tn)
+      fn_rate = fn / (fn + tp)
+      fp_rate_list.append(fp_rate)
+      fn_rate_list.append(fn_rate)
+
+    # Plot the FPR vs FNR curve
+    plt.plot(fp_rate_list, fn_rate_list, label='FPR vs FNR', color='red')
+
+    # Add the point where FPR=0 and FNR=0 as a circle marker
+    plt.plot(0, 0, 'o', markersize=8, color='green')
+
+    # Add a title, axis labels, and legend
+    plt.title('False Positive Rate vs False Negative Rate')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('False Negative Rate')
+    plt.legend(loc='best')
+
+    # Set the limits of the plot
+    plt.xlim(0, 0.06)
+    plt.ylim(0, 0.2)
+
+    # Show the plot
+    plt.show()
+  
+  def __save_pred_prob(self, adaptive_model, dir=""):
+    y_pred_prob = adaptive_model.model.predict_proba(
+      self.feature_extractors_dict['tf-idf_ngram'].features['test'])[:, 1]
+    #result = (y_pred_prob, self.data_manager.y_test)
+    y_test = self.data_manager.y_test
+    currentDateAndTime = datetime.now()
+    currentTime = currentDateAndTime.strftime("%y%m%d_%H%M%S")
+    file_name = Path(dir) / f'xgboost_pred_{currentTime}.csv'
+    df = pd.DataFrame({'y_pred_prob': y_pred_prob, 'y_test': y_test})
+    df.to_csv(file_name, index=False)
+    print(f'Prediction probabilities are saved to {file_name}')
+    return file_name
+
+  def __load_pred_prob(self, file_name):
+    df = pd.read_csv(file_name)
+    y_pred_prob = df['y_pred_prob'].values
+    y_test = df['y_test'].values
+    return y_test, y_pred_prob
+
+
+  def run_tests(self, feature_methods, classic_models, ensemble_models):
+    self.__features_models_cartesian_tests(feature_methods, classic_models)
+    self.__run_ensemble_tests(ensemble_models)
+    #self.__adaptive('tf-idf_ngram', 'xgboost', threshold=0.5)
+    adaptive_model = self.__adaptive('tf-idf_ngram', 'xgboost', threshold=0.3)
+    file_name = self.__save_pred_prob(adaptive_model,dir=Path(self.config['results']['dir']))
+    # TODO: save pred_prob and open it in display results ipython file.
+    # TODO: plot ROC for 0.0001 values as well(may be logarithmic in x axis?)
+    # TODO: Make a function to calculate the estimated speed vs Recall.
+    #self.__plot_roc3(file_name)
+
+    self.__save_results(Path(self.config['results']['dir']))
+    self.results = []
